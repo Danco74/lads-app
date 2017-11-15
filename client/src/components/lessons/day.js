7246381dba29034163a7efc834c88cce6ceb440c
Yeshayahu Wasserman <Shy.Wasserman@gmail.com>
2017-11-16 00:56:29 +0200

    bla

--- client/src/components/lessons/day.js
+++ client/src/components/lessons/day.js
@@ -20,7 +20,7 @@ class Day extends Component {
                 <div className="day-topic">
                     {this.props.days.map((day, index) =>
                         <div key={index}>
-                        <Link className="weekTopic link" to="/lads/lesson/1" >
+                        <Link className="weekTopic link" to={`/lads/lesson/${lesson.id}`} >
                         {/* <Link to={`${this.props.routeprops.url}/`} > */}
                                     {day.Topic.toUpperCase()}
                             </Link>
